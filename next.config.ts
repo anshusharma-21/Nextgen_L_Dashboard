30d64567a9a8c7a2e6895fbdc0015204ccef268b
anshusharma-21 <anshusharma21aaj@gmail.com>
2026-06-04 15:46:56 +0530

    build: production fix v2 layout configuration

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   experimental: {
-//     // Yeh built-in flag Turbopack engine ko bina kisi external PostCSS config ke automatic Tailwind compile karne par force karega
-//     optimizeCss: true,
-//   },
-// };
-
-// export default nextConfig;
-
-
-
-
-
-
-
-
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
